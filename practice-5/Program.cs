c276b27bf9b0a2ef3e3b24747a43ad2352f63e06
Zahar <60888814+ZaharAmbrozyak@users.noreply.github.com>
2026-03-05 21:53:20 +0200

    Finishing work with assignment. Wrote an example in Program.cs

--- practice-5/Program.cs
+++ practice-5/Program.cs
@@ -4,6 +4,23 @@ internal static class Program
 {
     private static void Main()
     {
+        var registry = new UniversityRegistry();
+
+        var professor1 = new Professor("Chopyuk", "1");
+        var gradStudent1 = new GraduateStudent("Zakhar", "2");
+        var undergradStudent1 = new UndergraduateStudent("Ivan", "3");
+        var undergradStudent2 = new UndergraduateStudent("Nazar", "4");
         
+        registry.AddMember(professor1);
+        registry.AddMember(gradStudent1);
+        registry.AddMember(undergradStudent1);
+        registry.AddMember(undergradStudent2);
+        
+        registry.ExecuteAllDuties();
+        registry.ExecuteAllDuties();
+        
+        undergradStudent1.PerformDuties();
+        
+        registry.GetMemberStatistics();
     }
 }
